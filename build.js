259bec389818b5bca5a95ec9b94fb8c0a524dbb5
henrikberglind <81242207+henrikberglind@users.noreply.github.com>
2026-09-18 13:51:49 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
-const gtfsToGeoJSON = require('gtfs-to-gesojson');
+const gtfsToGeoJSON = require('gtfs-to-geojson');
 const fs = require('fs');
 
+// Ersätt platshållaren i config med vår hemliga API-nyckel
 let configText = fs.readFileSync('./config.json', 'utf8');
 const apiKey = process.env.TRAFIKLAB_API_KEY;
 configText = configText.replace('__API_KEY__', apiKey);
@@ -10,10 +11,10 @@ console.log('Startar konvertering av JLT GTFS till GeoJSON...');
 
 gtfsToGeoJSON(config)
   .then(() => {
-    console.log('GeoJSON-filer har skapats i /output.');
-      process.exit(0);
+    console.log('GeoJSON-filer har skapats i /output!');
+    process.exit(0);
   })
   .catch(err => {
-    console.error:', err);
+    console.error('Ett fel uppstod:', err);
     process.exit(1);
   });
